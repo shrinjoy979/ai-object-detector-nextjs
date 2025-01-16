65c324e40dd601818098fb9b1aae4c152701ba65
Shrinjoy Saha <shrinjoysaha@Shrinjoys-MacBook-Air.local>
2025-01-17 05:20:30 +0530

    small change

--- app/page.js
+++ app/page.js
@@ -7,6 +7,7 @@ export default function Home() {
       <h1 className="gradient-title font-extrabold text-3xl md:text-6xl lg:text-8xl tracking-tighter md:px-6 text-center">
         Thief Detection Alarm
       </h1>
+      <p className="pt-1">The camera must be activated to enable object detection</p>
       <ObjectDetection />
     </main>
   );
